2dbf66ce8945f6042a117d8965fa9a3a23834238
SunwooKim11 <seonu2001@naver.com>
2020-06-26 12:45:06 +0900

    Issue #4 Upload calc dic(wsgi h.w)

---
+++ calc/cal.py
@@ -0,0 +1,20 @@
+from cgi import parse_qs
+from template_cal import html
+
+def application(environ, start_response):
+    d = parse_qs(environ['QUERY_STRING'])
+    a = d.get('a', [''])[0]
+    b = d.get('b', [''])[0]  
+    S=0 
+    P=0
+    if '' not in [a, b]:
+        a, b = int(a), int(b)
+
+	S=a+b
+	P = a*b
+    response_body = html+'sum: '+str(S)+', product:'+str(P)
+    start_response('200 OK', [
+        ('Content-Type', 'text/html'),
+        ('Content-Length', str(len(response_body)))
+    ])
+    return [response_body]
---
+++ calc/template_cal.py
@@ -0,0 +1,12 @@
+html = b"""
+<html>
+    <body>
+        <form action="">
+            a = <input type="number" name="a"><br><br>
+            b = <input type="number" name="b"><br><br>
+            <input type="submit">
+        </form>
+
+    </body>
+</html>
+"""
